ea713ca126aadb4cab6087e370a9f7a8b1f6e08c
sajib925 <sajibahmed1004@gmail.com>
2024-10-24 15:02:22 +0600

    contact model updated

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-
 class ContactUs(models.Model):
-    name = models.CharField(max_length = 30)
-    phone = models.CharField(max_length = 12)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    subject = models.CharField(max_length=30, null=True, blank=True)  
+    phone = models.CharField(max_length=12, null=True, blank=True)    
     problem = models.TextField()
-    
+
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name_plural = "Contact Us"
