c7e3c6b3a501ed563f5ccde1263ebe905fd972d8
sajib925 <sajibahmed1004@gmail.com>
2024-10-25 23:45:39 +0600

    campaign serializers update

--- campaign/serializers.py
+++ campaign/serializers.py
@@ -3,11 +3,27 @@ from .models import CampaignModel, VaccinesModel
 
 
 
+# class CampaignModelSerializer(serializers.ModelSerializer):
+#     doctor_username = serializers.ReadOnlyField(source='doctor.username')
+#     class Meta:
+#         model = CampaignModel
+#         fields = ['id', 'name', 'image', 'description', 'created_at', 'updated_at', 'doctor_username']
+
 class CampaignModelSerializer(serializers.ModelSerializer):
     doctor_username = serializers.ReadOnlyField(source='doctor.username')
+
     class Meta:
         model = CampaignModel
         fields = ['id', 'name', 'image', 'description', 'created_at', 'updated_at', 'doctor_username']
+
+    def create(self, validated_data):
+        doctor = self.context['request'].user
+        validated_data.pop('doctor', None)
+        campaign = CampaignModel.objects.create(**validated_data)
+        campaign.doctor = doctor
+        campaign.save()
+        return campaign
+
 
 
 class VaccinesModelSerializer(serializers.ModelSerializer):
